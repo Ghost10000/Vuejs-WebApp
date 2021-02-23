813bcc2020d238ba5ff054e42105f3b41d14b4e5
Ghost10000 <bernardshirima@gmail.com>
2021-02-23 11:49:14 +0200

    first commit for vuejs web app

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
+   
+    
+  </div>
+</template>
+
+<script>
+
+export default {
+  
+}
+</script>
+
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #000;
+  
+}
+
+#nav {
+  padding: 30px;
+  background: #333;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #fff;
+  text-decoration: none;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="home">
+    <h1>Common Quotes</h1><hr><br>
+    <div class="container"  v-for="quote in quotes" :key="quote.id">
+        
+        <p>{{ quote.text }}</p>
+        <h2>{{ quote.author }}</h2>
+        
+    </div>
+    
+    <footer>
+      <p>Copyright &copy; 2021 </p>
+    </footer>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+ 
+  data(){
+    return{
+      quotes: []
+
+    }
+    
+  },
+
+  created(){
+    axios.get('https://type.fit/api/quotes')
+    .then(res => this.quotes = res.data)
+    .catch(err => console.log(err));
+    }
+
+  }
+
+</script>
+
+<style>
+.container{
+  background: #dd3;
+}
+
+footer{
+  position: relative;
+}
+
+footer p{
+  position: absolute;
+   bottom: -2%;
+   background: #333;
+   width: 100%;
+   color: #fff;
+   padding: 10px;
+}
+
+</style>
+
